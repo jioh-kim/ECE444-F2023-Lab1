4aa899376d2fbd44828aefc0b780e83c45d69db7
Ji-Oh Kim <jiohkim1103@gmail.com>
2023-09-19 11:47:04 -0400

    forgot to add .py code in my previous commit

--- hello.py
+++ hello.py
@@ -1,13 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+# from flask_wtf import Form
+# from wtforms import StringField, SubmitField
+# from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
-@app.route('/')
+# class NameForm(Form):
+#     name = StringField('What is your name?', validators=[DataRequired()]) 
+#     submit = SubmitField('Submit')
+
+# @app.route('/')
+# def index():
+#     return '<h1>Hello World!</h1>' 
+
+@app.route('/user/<name>') 
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.route('/') 
 def index():
-    return '<h1>Hello World!</h1>' 
+    return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<Jioh>') 
-def user(Jioh):
-    return '<h1>Hello, %s!</h1>' % Jioh
-
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+    
+@app.errorhandler(404) 
+def page_not_found(e):
+    return render_template('404.html'), 404
