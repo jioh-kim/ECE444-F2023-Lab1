3b016af502399fcc5693b840032991ce9c79678c
Ji-Oh Kim <jiohkim1103@gmail.com>
2023-09-20 23:06:09 -0400

    Added error messages for invalid email and added message flashing for updating information

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -13,7 +13,7 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()]) 
-    email = StringField('What is your UofT Email address?', validators=[DataRequired()]) 
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()]) 
     submit = SubmitField('Submit')
 
 # @app.route('/')
@@ -23,10 +23,21 @@ class NameForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST']) 
 def index():
     form = NameForm()
+    
     if form.validate_on_submit():
+        
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+            
         session['email'] = form.email.data
         return redirect(url_for('index'))
+    
     return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>') 
