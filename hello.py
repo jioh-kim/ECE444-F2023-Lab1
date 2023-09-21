6106e6ea41294f6af01da25cdf283b7b9b428f36
Ji-Oh Kim <jiohkim1103@gmail.com>
2023-09-20 22:19:04 -0400

    Added form boxes for name and email, and redirect/render template for new updated name and email

--- hello.py
+++ hello.py
@@ -1,30 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-# from flask_wtf import Form
-# from wtforms import StringField, SubmitField
-# from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
-# class NameForm(Form):
-#     name = StringField('What is your name?', validators=[DataRequired()]) 
-#     submit = SubmitField('Submit')
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()]) 
+    email = StringField('What is your UofT Email address?', validators=[DataRequired()]) 
+    submit = SubmitField('Submit')
 
 # @app.route('/')
 # def index():
-#     return '<h1>Hello World!</h1>' 
+#     return '<h1>Hello World!</h1>'
+
+@app.route('/', methods=['GET', 'POST']) 
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        session['name'] = form.name.data
+        session['email'] = form.email.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 @app.route('/user/<name>') 
 def user(name):
     return render_template('user.html', name=name)
-
-@app.route('/') 
-def index():
-    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.errorhandler(500)
 def internal_server_error(e):
